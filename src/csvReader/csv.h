b4e1711aeec5024fbd02cebe5ad6d15e8b5e5f3b
Mohammed Safwat <Mohammed_ElAfifi@yahoo.com>
2013-06-04 19:33:14 +0200

    - creating a doxyGen file for documentation - moving all sources to a separate source folder - adding documentation comments in the source code

--- src/csvReader/csv.h
+++ src/csvReader/csv.h
@@ -3,8 +3,23 @@
 #include <boost\algorithm\string\regex.hpp>
 #include <fstream>
 
+/**
+  * @brief Utilities for processing csv files
+  */
 namespace CvsUtils
 {
+    /**
+      * @brief Appends a field to the current active record
+      *
+      * This function opens the given csv file, parses the records in it, and
+      * the closes it.
+      *
+      * @tparam    T        output record iterator, where a record is
+      *                     represented by a field vector
+      * @param[in]  csvName csv file name
+      * @param[out] records output iterator to a record, where a single record
+      *                     is represented by a field vector
+      */
     template < class T >
     void ReadCsvFile(const _TCHAR csvName[], T records)
     {
--- src/csvReader/csvReader.cpp
+++ src/csvReader/csvReader.cpp
@@ -1,9 +1,21 @@
 // csvReader.cpp : Defines the entry point for the console application.
 //
+/**
+ * @file
+ * @brief Main entry point
+ *
+ * This file defines the entry point for the console application.
+ */
 
 #include "stdafx.h"
 #include "csv.h"
 
+/**
+* @brief Executes the program
+*
+* @param[in] argc number of command-line arguments
+* @param[in] argv command-line arguments
+*/
 int _tmain(int argc, _TCHAR* argv[])
 {
     std::list< std::vector< std::string > > records;
