677a12796b124e6d55efb5c069690a723b0b95e8
Mohammed Safwat <Mohammed_ElAfifi@yahoo.com>
2014-01-02 22:06:51 +0200

    - returning the last iterator of extracted csv records from csv reading functions - adding a function to allow reading from an arbitrary input stream(not necessarily a file)

--- src/csv.h
+++ src/csv.h
@@ -15,6 +15,36 @@
 namespace CsvUtils
 {
     /**
+     * @brief Reads records in a csv stream
+     *
+     * @tparam     T       output record iterator, where a record is represented
+     *                     by a field vector
+     * @param[in]  csvSrc  csv source stream
+     * @param[out] records output iterator to a record, where a single record is
+     *                     represented by a field vector
+     * @return             iterator past the last record
+     */
+    template < class T >
+    T ReadCsv(std::istream& csvSrc, T records)
+    {
+        using std::string;
+        string line;
+        std::vector< string > recParts;
+        static const char sep[] = ",";
+
+        while (std::getline(csvSrc, line))
+        {
+
+            boost::split_regex(recParts, line, boost::regex(sep));
+            *(records++) = recParts;
+
+        }
+
+        return records;
+
+    }
+
+    /**
      * @brief Reads records in a csv file
      *
      * This function opens the given csv file, parses the records in it, and
@@ -25,25 +55,16 @@ namespace CsvUtils
      * @param[in]  csvName csv file name
      * @param[out] records output iterator to a record, where a single record is
      *                     represented by a field vector
+     * @return             iterator past the last record
      */
     template < class T >
-    void ReadCsvFile(const _TCHAR csvName[], T records)
+    T ReadCsvFile(const _TCHAR csvName[], T records)
     {
-        using std::string;
         std::ifstream csvFile(csvName);
-        string line;
-        std::vector< string > recParts;
-        static const char sep[] = ",";
 
-        while (std::getline(csvFile, line))
-        {
-
-            boost::split_regex(recParts, line, boost::regex(sep));
-            *(records++) = recParts;
-
-        }
-
+        records = ReadCsv(csvFile, records);
         csvFile.close();
+        return records;
 
     }
 }
